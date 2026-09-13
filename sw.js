a6bba79725e89b06d4f15861ebc65b9d762b9aaa
mirzakashif434-source <mirzakashif434@gmail.com>
2026-09-13 16:39:45 +0300

    Invalidate old Challenge Arena cache v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='challenge-arena-v8';
+const CACHE='challenge-arena-v9';
 const ASSETS=['./','./index.html','./style.css','./script.js','./arena-rush.js','./arena-rush-v2.js','./arena-rush-v3.js','./manifest.webmanifest','./privacy.html','./terms.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
